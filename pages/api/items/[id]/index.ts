01d96afeeabd6297005a7980ff2c667c2051fcbd
ngoctran <ngoctq110309@gmail.com>
2021-11-12 10:19:22 +0700

    first nft

---
+++ pages/api/items/[id]/index.ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { useRouter } from 'next/dist/client/router';
+
+type Data = {
+  title: string,
+  type: any,
+  properties: any,
+  tokenId: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  const id = Number(req.query.id);
+  const id_image = (id) % 10;
+  const host = req.headers.host;
+
+
+  res.status(200).json({
+    title: "Asset Metadata",
+    type: "object",
+    properties: {
+      name: {
+        "type": "string",
+        "description": "Identifies the asset to which this NFT represents"
+      },
+      "description": {
+        "type": "string",
+        "description": "Describes the asset to which this NFT represents"
+      },
+      "image": {
+        "type": "string",
+        "description": `${host}/nft/Floki-${id_image}.png`
+      }
+    },
+    tokenId: `${id}`
+
+  })
+}
